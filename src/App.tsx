b431cc72b4413ef5b2cc92de6ccd9d24eab7d690
kalanad99 <kalanadananjay855@gmail.com>
2024-02-13 20:21:57 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Router,Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
+export default function App() {
+  return (
+    <Container>
+      <h1 className="text-3xl font-bold italic">
+        Hello world!
+      </h1>
+    </Container>
+    
+  )
+}
