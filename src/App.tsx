7a1456e4e74548b50d5ae1f8a151a7e81f70b11b
kalanad99 <kalanadananjay855@gmail.com>
2024-02-27 02:16:41 +0530

    Home page route done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Router,Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import Home from "./pages/Home"
 
 export default function App() {
   return (
     <Container>
-      <h1 className="text-3xl font-bold italic">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/About" element={<About/>}></Route>
+        <Route path="/Products" element={<Products/>}></Route>
+        <Route path="/Admin" element={<Admin/>}></Route>
+        <Route path="/Contact" element={<Contact/>}></Route>
+      </Routes>
     </Container>
     
   )
 }
- 
+  
